a0df299dc0876f4414a246925ff6bf72ff2a17cb
Akankshi Priya <akankshipriya24@gmail.com>
2022-09-02 19:47:35 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,15 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace fashion
+namespace reftype
 {
+    public class Student
+    {
+
+        public string StudentName { get; set; }
+
+    }
+
     class Program
     {
+        //value type
+        static void ChangeValue(int x)
+        {
+            x = 200;
+            Console.WriteLine(x);
+        }
+
+        //reference type
+        public static void ChangeReferenceType(Student std2)
+        {
+            std2.StudentName = "Akankshi";
+        }
         static void Main(string[] args)
         {
+            // value type
+            int i = 100;
+            Console.WriteLine(i);
+            ChangeValue(i);
+            Console.WriteLine(i);
+
+            //reference type
+            Student std1 = new Student();
+            std1.StudentName = "Priya";
+            ChangeReferenceType(std1);
+            Console.WriteLine(std1.StudentName);
+            Console.ReadLine();
+
         }
     }
 }
